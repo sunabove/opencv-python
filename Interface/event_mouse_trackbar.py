eadfd68e029f88d761bf51fde7d5364ab876b7b7
sunabove <sunabove@nate.com>
2024-06-02 23:24:07 +0900

    **

--- Interface/event_mouse_trackbar.py
+++ Interface/event_mouse_trackbar.py
@@ -1,27 +1,25 @@
 import numpy as np
 import cv2
 
+# 트랙바 콜백
 def onChange(value):
-    global image, title
+    pass
+pass
 
-    add_value = value -int(image[0][0])
-    image = image + add_value
-    cv2.imshow(title, image)
-
+# 마우스 콜백
 def onMouse(event, x, y, flags, param):
     global image, bar_name
 
-    if event == cv2.EVENT_RBUTTONDOWN:
-        if (image[0][0] < 246):
-            image = image + 10
-            cv2.setTrackbarPos(bar_name, title, image[0][0])
-            cv2.imshow(title, image)
-
-    elif event == cv2.EVENT_LBUTTONDOWN:
-        if (image[0][0] >=10):
-            image = image - 10
-            cv2.setTrackbarPos(bar_name, title, image[0][0])
-            cv2.imshow(title, image)
+    if event == cv2.EVENT_RBUTTONDOWN: # 오른쪽 마우스 클릭시
+        image[ : ] = cv2.getTrackbarPos( bar_name, title ) + 10
+        cv2.setTrackbarPos(bar_name, title, image[0][0] + 10)
+        cv2.imshow(title, image)
+    elif event == cv2.EVENT_LBUTTONDOWN: # 왼쪽 마우스 클릭시
+        image[ : ] = cv2.getTrackbarPos( bar_name, title ) - 10
+        cv2.setTrackbarPos(bar_name, title, image[0][0])
+        cv2.imshow(title, image)
+    pass
+pass
 
 image = np.zeros((300, 500), np.uint8) # 영상 생성
 title = "Mouse & Trackbar  event"
--- Interface/event_trackbar.py
+++ Interface/event_trackbar.py
@@ -1,16 +1,14 @@
 import numpy as np
 import cv2
 
-def onChange(value): # 트랙바 콜백 함수
-    global image, title # 전역 변수
-
-    add_value = value -int(image[0][0]) # 트랙바 값과 영상 화소값의 차
-    image = image + add_value # 행렬과 스칼라 덧셈
-    cv2.imshow(title, image)
-
 image = np.zeros((300, 500), np.uint8) # 영상 생성
-
 title = "trackbar event"
+
+def onChange(value): # 트랙바 콜백 함수
+    image[:] = value # 트랙바의 값으로 이미지 값을 설정
+    cv2.imshow(title, image)
+pass
+
 cv2.imshow(title, image)
 
 cv2.createTrackbar("Brightness", title, image[0][0], 255, onChange) # 트랙바 콜백
