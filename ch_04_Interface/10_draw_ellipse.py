2a2070d29f54cf3e5631712aec50d717be18a270
sunabove <sunabove@nate.com>
2024-06-03 18:20:11 +0900

    Update 10_draw_ellipse.py

--- ch_04_Interface/10_draw_ellipse.py
+++ ch_04_Interface/10_draw_ellipse.py
@@ -8,15 +8,18 @@ orange, blue, white = (255, 165, 0), (0, 0, 255), (255,255,255)
 image = np.full((300, 700, 3), white, np.uint8)
 
 pt1, pt2 = (180, 150), (550, 150)   # 타원 중심점
-size = (120, 60)                    # 타원 크기 - 반지름 값임
+size = (120, 60)                    # 타원 크기 - 반지름
 
 # 타원의 중심점(2화소 원) 표시
 cv2.circle(image, pt1, 1, 0, 2)
 cv2.circle(image, pt2, 1, 0, 2)
 
-cv2.ellipse(image, pt1, size,  0, 0, 360, blue, 1)      # 타원 그리기
+# 타원 그리기
+cv2.ellipse(image, pt1, size,  0, 0, 360, blue, 1)
 cv2.ellipse(image, pt2, size, 90, 0, 360, blue, 1)
-cv2.ellipse(image, pt1, size,  0, 30, 270, orange, 4)   # 호 그리기
+
+# 호 그리기
+cv2.ellipse(image, pt1, size,  0, 30, 270, orange, 4)
 cv2.ellipse(image, pt2, size, 90,-45,  90, orange, 4)
 
 plt.imshow(image) 
