1506f7b0ec8c12da5a6a1a2d91899d5183dbe940
sunabove <sunabove@nate.com>
2024-06-14 12:01:28 +0900

    **

--- ch_04_Interface/25_matplatlib_imshow.py
+++ ch_04_Interface/25_matplatlib_imshow.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 
 # 현재 소스 파일의 폴더 경로를 가져옵니다.
-dir = Path(__file__).resolve().parent
+dir = Path( __file__ ).resolve().parent
 
 # 이미지를 불러옵니다.
 image = cv2.imread(dir.joinpath("img/matplot.jpg"), cv2.IMREAD_COLOR)
@@ -19,14 +19,14 @@ rgb_img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 # 이미지를 그레이스케일로 변환합니다.
 gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# 첫 번째 figure 생성 (크기: 3x4 인치)
+# 첫 번째 figure(챠트, 그라프, 플롯) 생성 (크기: 3x4 인치)
 fig = plt.figure(num=1, figsize=(3, 4))
 
 # 원본(BGR) 이미지를 표시합니다.
 plt.imshow(image)
 plt.title('figure1- original(bgr)')
 plt.axis('off')  # 축을 표시하지 않습니다.
-plt.tight_layout() # 레이아웃을 조정하여 그림 요소가 겹치지 않도록 합니다.
+plt.tight_layout() # 레이아웃을 조정하여 챠트 요소가 겹치지 않도록 합니다.
 
 # 두 번째 figure 생성 (크기: 6x4 인치)
 fig = plt.figure(num=2, figsize=(6, 4))
