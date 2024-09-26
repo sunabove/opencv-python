c972288be432b903ad043db8689e7ff8ba51676d
sunabove <sunabove@nate.com>
2024-09-27 08:38:57 +0900

    **

--- ch_05_numpy_array/102_1차원 배열 초기화 01.py
+++ ch_05_numpy_array/102_1차원 배열 초기화 01.py
@@ -1,10 +1,12 @@
 # coding: utf-8
 import numpy as np
 
+# 1차원 배열 초기화
 a = np.array( [ 1., 2., 3. ] )
 b = np.zeros(3, int)
 c = np.zeros_like(a)
 
+# 주요 속성 값 출력
 print( a.dtype, a.shape )
 print( b.dtype, b.shape )
 print( c.dtype, c.shape )
